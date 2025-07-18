c5db5714a20dd3ad58826a72564994f6c6bcb832
João Nahid <joaonahiddev@gmail.com>
2025-07-18 16:34:27 -0300

    :sparkles: Feature: create table in the db for the authors and populate it with seeder and factory

---
+++ database/factories/AuthorFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Author;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Author>
+ */
+class AuthorFactory extends Factory
+{
+
+    protected $model = Author::class;
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->name(),
+            'active' => $this->faker->boolean(75),
+            'created_at' => $this->faker->dateTimeBetween('-1 year', 'now'),
+            'updated_at' => now(),
+        ];
+    }
+}
