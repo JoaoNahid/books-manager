f3c58e427561a9b4dada661d0b9c3dd229653e77
João Nahid <joaonahiddev@gmail.com>
2025-07-18 17:13:19 -0300

    :sparkles: Feat: create tablefor books

---
+++ database/migrations/2025_07_18_193653_create_books_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        if (!Schema::hasTable('books')) {
+            Schema::create('books', function (Blueprint $table) {
+                $table->id();
+                $table->string('name', 255)->nullable(false);
+                $table->longText('description')->nullable(false);
+                $table->dateTime('publish_data')->nullable(false);
+                $table->unsignedBigInteger('author_id');
+                $table->foreign('author_id')->references('id')->on('authors')->restrictOnDelete();
+                $table->timestamps();
+            });
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('books');
+    }
+};
