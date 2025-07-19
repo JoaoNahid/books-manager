12a22399b4a01552010293b9f2581121e0217bf4
João Nahid <joaonahiddev@gmail.com>
2025-07-19 00:38:50 -0300

    :sparkles: Feature: create the book model

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -6,5 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class Book extends Model
 {
-    //
+    protected $fillable = [
+        'name',
+        'description',
+        'published_at',
+        'author_id',
+    ];
+
+    protected $casts = [
+        'published_at' => 'date',
+    ];
+
+    public function author() {
+        return $this->belongsTo(Author::class);
+    }
 }
