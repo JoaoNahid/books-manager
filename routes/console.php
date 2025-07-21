3ce5a01550ddecc2e5ffdfa175933c5379757640
João Nahid <joaonahiddev@gmail.com>
2025-07-20 22:22:09 -0300

    :sparkles: Feature:  schedule clear log command to everyday at midnight

--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,18 @@
 <?php
 
+use App\Models\Log;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Schedule::command('log:clear')->daily()->at('00:00')
+    ->onSuccess(function () {
+        Log::createLog('Logs cleared successfully.');
+    })
+    ->onFailure(function () {
+        Log::createLog('Failed to clear logs.');
+    });
