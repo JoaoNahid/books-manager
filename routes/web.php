e682a8846731aea822a1687791d3c8c75d7a258e
João Nahid <joaonahiddev@gmail.com>
2025-07-20 21:14:00 -0300

    :sparkles: Feature: pass middleware for the books route

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\BooksController;
+use App\Http\Middleware\CheckAdmin;
 use App\Livewire\Settings\Appearance;
 use App\Livewire\Settings\Password;
 use App\Livewire\Settings\Profile;
@@ -21,8 +22,10 @@ Route::middleware(['auth'])->group(function () {
     Route::get('settings/password', Password::class)->name('settings.password');
     Route::get('settings/appearance', Appearance::class)->name('settings.appearance');
 
-    Route::controller(BooksController::class)->group(function() {
-        Route::get('books', 'index')->name('books.index');
+    Route::middleware([CheckAdmin::class])->group(function () {
+        Route::controller(BooksController::class)->group(function() {
+            Route::get('books', 'index')->name('books.index');
+        });
     });
 });
 
