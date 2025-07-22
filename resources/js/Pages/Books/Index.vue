e21715021a86cc34250c24ce6e98b48f01c2d3a9
João Nahid <joaonahiddev@gmail.com>
2025-07-21 23:59:42 -0300

    :construction: Work in progress: creating the books main page

---
+++ resources/js/Pages/Books/Index.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import Dashboard from '@/Layouts/Dashboard.vue';
+
+</script>
+
+<template>
+    <Dashboard title="Livros">
+        <div class="p-6">
+            <h1 class="text-2xl font-bold">Livros</h1>
+            <p class="mt-4">Welcome to your dashboard!</p>
+        </div>
+    </Dashboard>
+</template>
