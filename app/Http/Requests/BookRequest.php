4e8440bd6309429f50bbdb7f5d8c11750941602f
João Nahid <joaonahiddev@gmail.com>
2025-07-23 00:46:59 -0300

    :sparkles: Feature: create request for books

---
+++ app/Http/Requests/BookRequest.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class BookRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => 'required|string|max:255',
+            'description' => 'required|string',
+            'published_at' => 'required|date',
+            'author_id' => 'required|exists:authors,id',
+            'image' => 'nullable|image|mimes:jpg,png|max:2048',
+            'id' => 'nullable|exists:books,id'
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'name.required' => 'O nome é obrigatório.',
+            'name.string' => 'O nome deve ser uma string.',
+            'name.max' => 'O nome não pode ter mais que 255 caracteres.',
+
+            'description.required' => 'A descrição é obrigatória.',
+            'description.string' => 'A descrição deve ser uma string.',
+
+            'published_at.required' => 'A data de publicação é obrigatória.',
+            'published_at.date' => 'A data de publicação deve ser uma data válida.',
+
+            'author_id.required' => 'O autor é obrigatório.',
+            'author_id.exists' => 'O autor informado não existe.',
+
+            'image.image' => 'O arquivo deve ser uma imagem.',
+            'image.mimes' => 'O arquivo deve ser uma imagem JPG ou PNG.',
+            'image.max' => 'A imagem deve ter no máximo 2MB.',
+        ];
+    }
+}
