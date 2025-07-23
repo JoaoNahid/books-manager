2b0974b8c493f3a3a5deaac74748f81c373840ca
João Nahid <joaonahiddev@gmail.com>
2025-07-23 14:06:41 -0300

    :hammer: Refactor: remove file when delete book

--- app/Http/Controllers/BooksController.php
+++ app/Http/Controllers/BooksController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Requests\BookRequest;
 use App\Models\Author;
 use App\Models\Book;
+use App\Models\FileUpload;
 use Inertia\Inertia;
 
 class BooksController extends Controller
@@ -16,7 +17,6 @@ class BooksController extends Controller
         ]);
     }
 
-    // TODO: arrumar retorno - página precisa recarregar para exibir item recém cadastrado/deletado
     public function store(BookRequest $request) {
         Book::post($request->validated());
         return redirect()->route('books.index');
@@ -24,6 +24,9 @@ class BooksController extends Controller
 
     public function deleteBook($id) {
         $book = Book::findOrFail($id);
+        if ($book->image) {
+            FileUpload::deleteImage($book->image);
+        }
         $book->delete();
 
         return back()->with('success', 'Livro excluído com sucesso!');
