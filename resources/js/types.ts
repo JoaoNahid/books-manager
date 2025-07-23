0f8cd7b2c42c7761de59d5a9530a768adf556659
João Nahid <joaonahiddev@gmail.com>
2025-07-23 00:44:58 -0300

    :sparkles: Feature: create types

---
+++ resources/js/types.ts
@@ -0,0 +1,23 @@
+export interface Book {
+    id: number;
+    name: string;
+    description: string;
+    author: Author;
+    published_at: string;
+    image: string | null;
+}
+
+export interface BookFormProps {
+    id: number;
+    name: string;
+    author_id: number;
+    published_at: Date | string;
+    image: string;
+    [key: string]: any
+}
+
+export interface Author {
+    id: number;
+    name: string;
+    active: boolean;
+}
