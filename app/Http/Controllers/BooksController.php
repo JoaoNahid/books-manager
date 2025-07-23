82069fd684006a278c43b91f909400625435e4e3
João Nahid <joaonahiddev@gmail.com>
2025-07-23 00:48:54 -0300

    :construction: Work in progress: create  methods for post and delete

--- app/Http/Controllers/BooksController.php
+++ app/Http/Controllers/BooksController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\BookRequest;
+use App\Models\Author;
 use App\Models\Book;
 use Inertia\Inertia;
 
@@ -9,7 +11,21 @@ class BooksController extends Controller
 {
     public function index() {
         return Inertia::render('Books/Index', [
-            'books' => Book::all()
+            'books' => Book::getBooks()->paginate(10),
+            'authors' => Author::getActiveAuthors(),
         ]);
+    }
+
+    // TODO: arrumar retorno - página precisa recarregar para exibir item recém cadastrado/deletado
+    public function store(BookRequest $request) {
+        Book::post($request->validated());
+        return redirect()->route('books.index');
+    }
+
+    public function deleteBook($id) {
+        $book = Book::findOrFail($id);
+        $book->delete();
+
+        return back()->with('success', 'Livro excluído com sucesso!');
     }
 }
