8561c2ea21c35b5d1edbd6e83703b1c725bb1c5c
João Nahid <joaonahiddev@gmail.com>
2025-07-23 00:49:55 -0300

    :sparkles: Feature: create method to get and post books

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -21,4 +21,27 @@ class Book extends Model
     public function author() {
         return $this->belongsTo(Author::class);
     }
+
+    public static function getBooks() {
+        return self::select()->with('author');
+    }
+
+    public static function post($data): self|false {
+        try {
+            if (isset($data['image'])) {
+                $uploadedImage = FileUpload::uploadImage($data['image']);
+                unset($data['image']);
+            }
+            $book = self::query()->updateOrCreate(['id' => $data['id'] ?? null], $data);
+            if ($book && $book->image) {
+                FileUpload::deleteImage($book->image);
+                $book->image = $uploadedImage;
+                $book->save();
+            }
+            return $book;
+        } catch (\Exception $exception) {
+            Log::create('Fail to create or update a book register: ' . $exception->getMessage());
+            return false;
+        }
+    }
 }
